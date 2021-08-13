b4e01889b32f151365a1269848e6de7440f2a440
Jake <prevou15@gmail.com>
2021-08-13 04:07:46 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+function add(a, b) {
+};
+
+function subtract(a, b) {
+};
+
+function multiply(a, b) {
+};
+
+function divide(a, b) {
+};
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function increment(a) {
+    return a += 1;
+  };
+
+  function decrement(a) {
+    return a -= 1;
+  };
+
+
+function makeInt(n) {
+    let result = parseInt(n, 10);
+    return result;
+};
+
+function preserveDecimal(n) {
+    let result = parseFloat(n);
+    return result;
+};
+
+ 
